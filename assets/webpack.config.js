2b156f8e967223d01f6c6a16c2ac276491107a44
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-07-23 19:43:05 -0700

    Add webpack rule for loading fonts

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -40,7 +40,17 @@ module.exports = (env, options) => {
             'css-loader',
             'sass-loader',
           ],
-        }
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '../fonts'
+            },
+          }],
+        },
       ]
     },
     plugins: [
